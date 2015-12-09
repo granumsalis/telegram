bca9f563a0f19b2b9cabc920539eefe3f8043db3
askopin <askopin@gmail.com>
2015-12-09 21:58:56 +0300

    a bit refactoring + admin list

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+    #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import logging
@@ -18,10 +18,10 @@ from pony.orm import *
 LAST_UPDATE_ID = None
 MESSAGE = "Пока никаких новостей...\nЯ буду присылать анонсы сюда и в канал @GranumSalis."
 MESSAGE_STOP = "Я умолкаю в этом чате! Может быть, за анонсами удобнее следить в канале @GranumSalis?.."
-MESSAGE_HELP = "/hello - Greetings\n/help - show this message\n/user_list - list of subscribers\n/send_broad <message> - send message to all users\n/send <username> <message> - send <message> to <username>\n/stop - exclude self from notification list"
+MESSAGE_HELP = "/hello - Greetings\n/help - show this message\n/stop - exclude self from notification list"
 MESSAGE_ALARM = "Аларм! Аларм!"
 CHAT_ID_ALARM = 79031498
-SEND_BROAD_CMD = '/send_broad'
+SEND_BROAD_CMD = '/send_broad '
 SEND_MSG_CMD = '/send'
 STOP_CMD = '/stop'
 SECRET_LIST_CMD = '/secret_list'
@@ -33,13 +33,12 @@ TELEGRAM_MSG_CHANNEL = '#telegram-messages'
 db = Database('sqlite', 'granum_salis.sqlite', create_db=True)
 class Chat(db.Entity):
     chat_id = Required(int, unique=True)
-    user_id = Optional(int, unique=True)
+    user_id = Required(int)
     open_date = Required(datetime)
     last_message_date = Optional(datetime)
     username = Optional(str)
     first_name = Optional(str)
     last_name = Optional(str)
-
 db.generate_mapping(create_tables=True)
 
 def main():
@@ -48,6 +47,11 @@ def main():
     parser = argparse.ArgumentParser(description="Telegram bot for GranumSalis")
     parser.add_argument("--logfile", type=str, default='log', help="Path to log file")
     args = parser.parse_args()
+
+    with open('.admin_ids') as f:
+        admin_ids = f.read().splitlines() 
+    if admin_ids == None:
+        admin_ids = list()
 
     # TODO: use it
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -64,13 +68,13 @@ def main():
 
     while True:
         try:
-            run(bot, args.logfile, slackbot)
+            run(bot, admin_ids, args.logfile, slackbot)
         except telegram.TelegramError as error:
             print "TelegramError", error
             time.sleep(1)
         except Exception:
             traceback.print_exc()
-            # bot.sendMessage(chat_id=CHAT_ID_ALARM, text=MESSAGE_ALARM)
+            bot.sendMessage(chat_id=CHAT_ID_ALARM, text=MESSAGE_ALARM)
             time.sleep(100)
 
 
@@ -104,12 +108,12 @@ def log_update(update, logfile, slackbot):
                 chat.last_message_date = datetime.now()
                 chat.username = message.from_user.username
         commit()
-    # slackbot.chat_post_message(TELEGRAM_MSG_CHANNEL, slack_text, as_user=True)
+    slackbot.chat_post_message(TELEGRAM_MSG_CHANNEL, slack_text, as_user=True)
 
 
 def send_broad(bot, text):
     with db_session:
-        for chat_id in select(chat.chat_id for chat in Chat):    # for chat in chats:
+        for chat_id in select(chat.chat_id for chat in Chat):
             try:
                 bot.sendMessage(chat_id=chat_id, text=text)
             except telegram.TelegramError:
@@ -118,7 +122,7 @@ def send_broad(bot, text):
 def print_userlist(bot, message):
     with db_session:
         chats_str = ''
-        for chat in select(chat for chat in Chat):    # for chat in chats:
+        for chat in select(chat for chat in Chat):
             try:
                 chats_str += 'user: {0} user_id: {1}\n'.format(chat.username, chat.user_id)
             except telegram.TelegramError:
@@ -151,13 +155,13 @@ def send_message(bot, message):
             else:
                 bot.sendMessage(chat_id=chat.chat_id, text = text)
 
-def run(bot, logfile, slackbot):
+def run(bot, admin_list, logfile, slackbot):
     global LAST_UPDATE_ID
     for update in bot.getUpdates(offset=LAST_UPDATE_ID, timeout=10):
         message = update.message
         # print message
         log_update(update, logfile, slackbot)
-
+        is_admin = str(message.from_user.id) in admin_list
         if message.text.startswith(SEND_BROAD_CMD):
             send_broad(bot, message.text[len(SEND_BROAD_CMD):])
         elif message.text.startswith(SEND_MSG_CMD):
@@ -166,29 +170,27 @@ def run(bot, logfile, slackbot):
             pass
         elif message.text == HELP_CMD:
             bot.sendMessage(chat_id=message.chat_id, text=MESSAGE_HELP)
-        elif message.text == STOP_CMD:
-            bot.sendMessage(chat_id=message.chat_id, text=MESSAGE_STOP)
-        elif message.text == SECRET_LIST_CMD:
-            pass
-            # timepad_token = open('.timepad_token').readline().strip()
-            # timepad_list_filename = timepad.save_list_to_file(timepad_token)
-            # bot.sendDocument(chat_id=message.chat_id, document=open(timepad_list_filename, 'rb'))
-            # os.remove(timepad_list_filename)
-        elif message.text == USER_LIST_CMD:
-            print_userlist(bot,message)
         elif message.text == HELLO_CMD:
             if message.from_user != None:
                 username = message.from_user.first_name + ' ' + message.from_user.last_name
             else:
                 username = 'anonymouse'
             bot.sendMessage(chat_id=message.chat_id, text="Hello " + username)
-        elif message.text != '':
+        elif message.text == STOP_CMD:
+            bot.sendMessage(chat_id=message.chat_id, text=MESSAGE_STOP)
+        elif is_admin and message.text == SECRET_LIST_CMD:
+            timepad_token = open('.timepad_token').readline().strip()
+            timepad_list_filename = timepad.save_list_to_file(timepad_token)
+            bot.sendDocument(chat_id=message.chat_id, document=open(timepad_list_filename, 'rb'))
+            os.remove(timepad_list_filename)
+        elif is_admin and message.text == USER_LIST_CMD:
+            print_userlist(bot,message)
+        elif is_admin and message.text != '':
             bot.sendMessage(chat_id=message.chat_id, text=MESSAGE)
         else:
             pass
             
         LAST_UPDATE_ID = update.update_id + 1
-
 
 if __name__ == '__main__':
     main()
