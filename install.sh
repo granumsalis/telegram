4ead8ecc41735689a282c82836f81c98bcd95fb3
Anton Afanasyev <fun@bk.ru>
2016-01-21 06:53:45 +0000

    Get rid of "pyasn1 import" warnings

--- install.sh
+++ install.sh
@@ -4,8 +4,12 @@ VENV_DIR=venv
 PIP=$VENV_DIR/bin/pip
 PIP_PACKAGES="requests pony parse pdfkit pyslack-real python-telegram-bot"
 
+sudo apt install python-pip python-virtualenv
+sudo apt-get --reinstall install python-pyasn1 python-pyasn1-modules
+
 virtualenv --no-site-packages --prompt="(granumsalis)" $VENV_DIR
 
+$PIP install pyasn1
 $PIP install $PIP_PACKAGES
 
 #source $VENV_DIR/bin/activate
